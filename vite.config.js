b2641693638f8667af3ddbe3e69e62397fb65aab
mattrossman <rossman@gatech.edu>
2021-10-21 15:05:16 -0400

    Enable vite library mode

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import path from "path"
+import { defineConfig } from "vite"
+import preact from "@preact/preset-vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/room.js"),
+      formats: ["es"],
+    },
+  },
 })
